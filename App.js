0f6e214acb0cefd92cafae4964e1a77a2640206f
Mohd Wasim <77122091+spideyhere@users.noreply.github.com>
2021-09-21 16:23:04 +0530

    Add files via upload
    Basic setup done

---
+++ App.js
@@ -0,0 +1,12 @@
+import Banner from './components/Banner';
+import './sass/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+    </div>
+  );
+}
+
+export default App;
